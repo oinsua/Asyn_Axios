9fa7d8af342bbf2463a9a9bac0bfc29d1756923b
¨Ossany_Insua_Morfa¨ <ossany.insua1982@gmail.com>
2020-07-07 11:51:29 -0700

    Axios Final

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ getName().then(res => console.log(res)); */
 
 getName().then(res => console.log(res)); */
 //Llamando a la funcion desde otra funcion para mostrar la necesidad de usar async cuando llamamos a API
-const getName = async () => {
+/* const getName = async () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve('Ossany');
@@ -39,6 +39,65 @@ const sayHello = async () => {
 }
 
 sayHello().then(res => console.log(res));
+ */
 
+//Ejemplo sencillo del ejercicio de promesas para llevarlo a Asyn Await
+const users = [
+    {
+        id:1,
+        name:'Ossany'
+    },
+    {
+        id:2,
+        name:'Esteban'
+    },
+    {
+        id:3,
+        name:'Maria Caridad'
+    }
+];
 
+const emails = [
+    {
+        id:1,
+        email:'ossany@gmail.com'
+    },
+    {
+        id:2,
+        email:'esteban@gmail.com'
+    }
+];
+
+const getUser = async (id) => {
+  const user = users.find((user) => user.id == id);
+
+    if(!user) throw new Error(`No existe un usuario con id ${id}`);
+    else return user;
+}
+
+const getEmail = async (user) => {
+   const email = emails.find((email) => email.id == user.id);
+   
+    if(!email) throw new Error(`No existe un email para ${user.name}`);
+    else return ({
+        id:user.id,
+        name: user.name,
+        email:email.email
+    });
+}
+
+//Debemos de hacer uso de una nueva funcion para poder ejecutar getEmail
+const getInfo = async (id) => {
+    try{
+        const user = await getUser(id);
+        return await getEmail(user);
+       }
+       catch(error){
+        console.log(error);
+       }
+}
+//Asi devuelve una promesa por lo tanto hay que usar el then
+//console.log(getInfo())
+
+getInfo(1).then(res => console.log(res));
 
---
+++ script_axios.js
@@ -0,0 +1,36 @@
+const button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    /* axios({
+        method:'GET',
+        url:'https://jsonplaceholder.typicode.com/users'
+    }).then(res => {
+        const list = document.getElementById('list');
+        const fragment = document.createDocumentFragment();
+        const res_data = res.data;
+        for (const data of res_data) {
+            const list_item = document.createElement('LI');
+            list_item.textContent = `${data.id} - ${data.name}`;
+            fragment.appendChild(list_item)
+        }
+        list.appendChild(fragment);
+    })
+    .catch(error => console.log(error)) */
+
+    //Otra manera mas optima de usarlo
+    const getUser = async () => {
+        const {data:user} = await axios.get('https://jsonplaceholder.typicode.com/users')
+        const list = document.getElementById('list');
+        const fragment = document.createDocumentFragment();
+        const res_data = user;
+        for (const data of res_data) {
+            const list_item = document.createElement('LI');
+            list_item.textContent = `${data.id} - ${data.name}`;
+            fragment.appendChild(list_item)
+        }
+        list.appendChild(fragment);
+    }
+
+    getUser();  
+
+})
